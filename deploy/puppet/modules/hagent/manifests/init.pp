dd8e95f634d0079fe6c579ffc4f0bcffb3483217
Andrey Scopenco <andrey@scopenco.net>
2014-11-03 11:40:19 -0500

    add init.d deps

--- deploy/puppet/modules/hagent/manifests/init.pp
+++ deploy/puppet/modules/hagent/manifests/init.pp
@@ -40,46 +40,47 @@ class hagent (
     # useful skeleton
     file {
         "/usr/local/hagent/.bashrc":
-            source  =>  "/etc/skel/.bashrc";
+            source => "/etc/skel/.bashrc";
 
         "/usr/local/hagent/.bash_profile":
-            source  =>  "/etc/skel/.bash_profile";
+            source => "/etc/skel/.bash_profile";
 
         "/usr/local/hagent/.bash_logout":
-            source  =>  "/etc/skel/.bash_logout";
+            source => "/etc/skel/.bash_logout";
     }
 
     file {
         ["/usr/local/hagent/lib", "/usr/local/hagent/sbin",
         "/usr/local/hagent/var", "/usr/local/hagent/etc", ]:
-            ensure  => "directory",
-            mode    => "0750";
+            ensure => "directory",
+            mode => "0750";
 
         ["/usr/local/hagent/sbin/hagent", ]:
-            mode    => "0750";
+            mode => "0750";
 
         "/usr/local/hagent/etc/hagent.conf":
-            source  =>  "puppet:///modules/hagent/hagent.conf",
-            notify  => Service["hagent"];
+            source => "puppet:///modules/hagent/hagent.conf",
+            notify => Service["hagent"];
 
         "/usr/local/hagent/etc/logrotate.conf":
-            source  =>  "puppet:///modules/hagent/hagent.logrotate";
+            source => "puppet:///modules/hagent/hagent.logrotate";
 
         "/etc/sysconfig/hagent":
-            source  =>  "puppet:///modules/hagent/hagent.sysconfig";
+            source => "puppet:///modules/hagent/hagent.sysconfig";
 
         "/etc/init.d/hagent":
-            owner  => "root",
-            group  => "root",
-            mode    => "0755",
-            source  =>  "puppet:///modules/hagent/hagent.initd",
+            owner => "root",
+            group => "root",
+            mode => "0755",
+            source => "puppet:///modules/hagent/hagent.initd",
+            require => File["/usr/local/hagent/var"],
             notify => Service["hagent"];
     }
 
     service {
          "hagent":
-            enable    => "true",
-            ensure    => "running",                                                                                                                                                   
-            require   => File["/etc/init.d/hagent"];
+            enable => "true",
+            ensure => "running",                                                                                                                                                   
+            require => File["/etc/init.d/hagent"];
      }
 }
